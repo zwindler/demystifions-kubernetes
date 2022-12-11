816318ef5e1abcb5771254d5eec15a4a74f1fb91
Denis GERMAIN <dt.germain@gmail.com>
2022-12-11 22:58:11 +0100

    fix

--- run/controller-manager.sh
+++ run/controller-manager.sh
@@ -3,6 +3,6 @@
 --kubeconfig admin.conf \
 --cluster-signing-cert-file=certs/ca.pem \
 --cluster-signing-key-file=certs/ca-key.pem \
---service-account-private-key-file=certs/kubernetes-key.pem \
+--service-account-private-key-file=certs/admin-key.pem \
 --use-service-account-credentials \
 --root-ca-file=certs/ca.pem
