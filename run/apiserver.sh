c8cdcd34cf7a23bd5468cc3a76b82d0d73e54416
Denis GERMAIN <dt.germain@gmail.com>
2022-12-11 22:04:13 +0100

    chore: update apiserver script

--- run/apiserver.sh
+++ run/apiserver.sh
@@ -2,12 +2,16 @@
 PATH=$PATH:$(pwd)
 
 kube-apiserver --allow-privileged --authorization-mode=Node,RBAC \
---client-ca-file=certs/ca.pem --etcd-servers=https://127.0.0.1:2379 \
---etcd-cafile=certs/ca.pem --etcd-certfile=certs/kubernetes.pem --etcd-keyfile=certs/kubernetes-key.pem \
+--client-ca-file=certs/ca.pem \
+--etcd-cafile=certs/ca.pem \
+--etcd-certfile=certs/kubernetes.pem \
+--etcd-keyfile=certs/kubernetes-key.pem \
+--etcd-servers=https://127.0.0.1:2379 \
 --service-account-key-file=certs/kubernetes.pem \
 --service-account-signing-key-file=certs/kubernetes-key.pem \
 --service-account-issuer=https://kubernetes.default.svc.cluster.local \
---tls-cert-file=certs/kubernetes.pem --tls-private-key-file=certs/kubernetes-key.pem
+--tls-cert-file=certs/kubernetes.pem \
+--tls-private-key-file=certs/kubernetes-key.pem
 
 kubectl version --short
 
