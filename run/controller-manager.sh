3f9df422002451d02831bafe1f640affe9d9a326
Denis GERMAIN <dt.germain@gmail.com>
2022-12-11 22:03:00 +0100

    chore: update controller manager script

--- run/controller-manager.sh
+++ run/controller-manager.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 PATH=$PATH:${pwd}
-kube-controller-manager --kubeconfig kube-controller-manager.conf \
---cluster-signing-cert-file=certs/ca.pem --cluster-signing-key-file=certs/ca-key.pem \
---service-account-private-key-file=certs/service-account-key.pem --use-service-account-credentials \
+kube-controller-manager \
+--kubeconfig admin.conf \
+--cluster-signing-cert-file=certs/ca.pem \
+--cluster-signing-key-file=certs/ca-key.pem \
+--service-account-private-key-file=certs/kubernetes-key.pem \
+--use-service-account-credentials \
 --root-ca-file=certs/ca.pem
