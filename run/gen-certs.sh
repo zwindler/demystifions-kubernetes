8fb18047717abb895f18080dfa724fcb0e9f6d32
Denis GERMAIN <dt.germain@gmail.com>
2022-12-11 22:54:17 +0100

    fix: gen-cert.sh

--- run/gen-certs.sh
+++ run/gen-certs.sh
@@ -61,9 +61,11 @@ cfssl gencert \
   -ca=ca.pem \
   -ca-key=ca-key.pem \
   -config=ca-config.json \
+  -hostname=127.0.0.1,10.0.0.1,kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster,kubernetes.svc.cluster.local \
   -profile=kubernetes \
   admin-csr.json | cfssljson -bare admin
-}
+done
+
 
 cd ..
 
