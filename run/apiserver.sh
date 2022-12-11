20ced4177deb1e92ba76b40bdd79cc1d2eb1f8db
Denis GERMAIN <dt.germain@gmail.com>
2022-12-11 22:52:03 +0100

    fix: certs

--- run/apiserver.sh
+++ run/apiserver.sh
@@ -3,14 +3,14 @@
 --authorization-mode=Node,RBAC \
 --client-ca-file=certs/ca.pem \
 --etcd-cafile=certs/ca.pem \
---etcd-certfile=certs/kubernetes.pem \
---etcd-keyfile=certs/kubernetes-key.pem \
+--etcd-certfile=certs/admin.pem \
+--etcd-keyfile=certs/admin-key.pem \
 --etcd-servers=https://127.0.0.1:2379 \
---service-account-key-file=certs/kubernetes.pem \
---service-account-signing-key-file=certs/kubernetes-key.pem \
+--service-account-key-file=certs/admin.pem \
+--service-account-signing-key-file=certs/admin-key.pem \
 --service-account-issuer=https://kubernetes.default.svc.cluster.local \
---tls-cert-file=certs/kubernetes.pem \
---tls-private-key-file=certs/kubernetes-key.pem
+--tls-cert-file=certs/admin.pem \
+--tls-private-key-file=certs/admin-key.pem
 
 # Try this
 # kubectl version --short
