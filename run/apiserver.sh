8fe6e175db30ebec89a2aa69e682b2ac34666069
Denis GERMAIN <dt.germain@gmail.com>
2022-12-11 22:01:11 +0100

    chore: use same certif for apiserver

--- run/apiserver.sh
+++ run/apiserver.sh
@@ -4,8 +4,8 @@ PATH=$PATH:$(pwd)
 kube-apiserver --allow-privileged --authorization-mode=Node,RBAC \
 --client-ca-file=certs/ca.pem --etcd-servers=https://127.0.0.1:2379 \
 --etcd-cafile=certs/ca.pem --etcd-certfile=certs/kubernetes.pem --etcd-keyfile=certs/kubernetes-key.pem \
---service-account-key-file=certs/service-account.pem \
---service-account-signing-key-file=certs/service-account-key.pem \
+--service-account-key-file=certs/kubernetes.pem \
+--service-account-signing-key-file=certs/kubernetes-key.pem \
 --service-account-issuer=https://kubernetes.default.svc.cluster.local \
 --tls-cert-file=certs/kubernetes.pem --tls-private-key-file=certs/kubernetes-key.pem
 
