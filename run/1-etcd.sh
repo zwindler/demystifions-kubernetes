ec7572bf9702fe2afdbbc51650d7ded63e870e05
Denis GERMAIN <dt.germain@gmail.com>
2023-03-28 09:32:56 +0200

    chore: simplify etcd script

--- run/1-etcd.sh
+++ run/1-etcd.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+CERTS_OPTS=--cert-file=certs/admin.pem \
+           --key-file=certs/admin-key.pem \
+           --trusted-ca-file=certs/ca.pem
+
 ./etcd --advertise-client-urls https://127.0.0.1:2379 \
+--listen-client-urls https://127.0.0.1:2379 \
 --client-cert-auth \
 --data-dir etcd-data \
---cert-file=certs/admin.pem \
---key-file=certs/admin-key.pem \
---listen-client-urls https://127.0.0.1:2379 \
---trusted-ca-file=certs/ca.pem
+$CERTS_OPTS
