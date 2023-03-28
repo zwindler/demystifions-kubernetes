9857f15026b1e00325644eb8ffcebcf576b2206c
Denis GERMAIN <dt.germain@gmail.com>
2023-03-28 10:12:34 +0200

    chore: clean controller-manager

--- run/3-controller-manager.sh
+++ run/3-controller-manager.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+CERTS_OPTS="--cluster-signing-cert-file=certs/ca.pem \
+            --cluster-signing-key-file=certs/ca-key.pem \
+            --service-account-private-key-file=certs/admin-key.pem \
+            --root-ca-file=certs/ca.pem"
+
 ./kube-controller-manager \
 --kubeconfig admin.conf \
---cluster-signing-cert-file=certs/ca.pem \
---cluster-signing-key-file=certs/ca-key.pem \
---service-account-private-key-file=certs/admin-key.pem \
---use-service-account-credentials \
---root-ca-file=certs/ca.pem
+--use-service-account-credentials
