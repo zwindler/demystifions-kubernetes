ab370b39da2a97589118484848d7e01ed4447307
Denis GERMAIN <dt.germain@gmail.com>
2023-03-28 10:37:22 +0200

    chore: clean kubelet

--- run/6-kubelet.sh
+++ run/6-kubelet.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+CONTAINERD_OPTS="--container-runtime=remote \
+                 --container-runtime-endpoint=unix:///var/run/containerd/containerd.sock"
+
 sudo ./kubelet \
---container-runtime=remote \
---container-runtime-endpoint=unix:///var/run/containerd/containerd.sock \
---fail-swap-on=false \
 --kubeconfig admin.conf \
 --register-node=true
+
+# If your server has swap (but we should have disabled it)
+#--fail-swap-on=false
