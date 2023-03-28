feb98a8a8c2b2266c1a90da95747bd2c6d9f0bb1
Denis GERMAIN <dt.germain@gmail.com>
2023-03-28 10:05:27 +0200

    chore: clean apiserver

--- run/2-apiserver.sh
+++ run/2-apiserver.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-./kube-apiserver --allow-privileged \
---authorization-mode=Node,RBAC \
---client-ca-file=certs/ca.pem \
---etcd-cafile=certs/ca.pem \
---etcd-certfile=certs/admin.pem \
---etcd-keyfile=certs/admin-key.pem \
---etcd-servers=https://127.0.0.1:2379 \
---service-account-key-file=certs/admin.pem \
---service-account-signing-key-file=certs/admin-key.pem \
---service-account-issuer=https://kubernetes.default.svc.cluster.local \
---tls-cert-file=certs/admin.pem \
---tls-private-key-file=certs/admin-key.pem
+CERTS_OPTS="--client-ca-file=certs/ca.pem \
+            --tls-cert-file=certs/admin.pem \
+            --tls-private-key-file=certs/admin-key.pem \
+            --service-account-key-file=certs/admin.pem \
+            --service-account-signing-key-file=certs/admin-key.pem \
+            --service-account-issuer=https://kubernetes.default.svc.cluster.local"
+
+
+ETCD_OPTS="--etcd-cafile=certs/ca.pem \
+           --etcd-certfile=certs/admin.pem \
+           --etcd-keyfile=certs/admin-key.pem \
+           --etcd-servers=https://127.0.0.1:2379"
+
+./kube-apiserver ${CERTS_OPTS} ${ETCD_OPTS} \
+            --allow-privileged --authorization-mode=Node,RBAC
 
 # Try this
 # kubectl version --short
