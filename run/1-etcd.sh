5a1c5ecf0d42c2744156271bef51660fda88aac6
Denis GERMAIN <dt.germain@gmail.com>
2023-03-28 09:50:06 +0200

    chore: etcd

--- run/1-etcd.sh
+++ run/1-etcd.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-CERTS_OPTS=--cert-file=certs/admin.pem \
+CERTS_OPTS="--client-cert-auth \
+           --cert-file=certs/admin.pem \
            --key-file=certs/admin-key.pem \
-           --trusted-ca-file=certs/ca.pem
+           --trusted-ca-file=certs/ca.pem"
 
-./etcd --advertise-client-urls https://127.0.0.1:2379 \
---listen-client-urls https://127.0.0.1:2379 \
---client-cert-auth \
---data-dir etcd-data \
-$CERTS_OPTS
+# By default, etcd will server HTTP, not HTTPS
+FORCE_HTTPS_OPTS="--advertise-client-urls https://127.0.0.1:2379 \
+                  --listen-client-urls https://127.0.0.1:2379"
+
+./etcd --data-dir etcd-data $CERTS_OPTS $FORCE_HTTPS_OPTS
