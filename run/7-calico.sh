e776a6041ad214842bd716651fb4a78e1bd3128b
Denis GERMAIN <dt.germain@gmail.com>
2023-03-28 11:51:04 +0200

    chore: set another subnet for calico

--- run/7-calico.sh
+++ run/7-calico.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 helm repo add projectcalico https://projectcalico.docs.tigera.io/charts
 kubectl create namespace tigera-operator
+
+cat > values.yaml <<EOF
+installation:
+  cni:
+    type: Calico
+  calicoNetwork:
+    ipPools:
+    - cidr: 10.244.0.0/16
+EOF
+
 helm install calico projectcalico/tigera-operator --version v3.24.5 --namespace tigera-operator
