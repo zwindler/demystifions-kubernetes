71df2a3c0ce1c1671b08e41361e7dc8be53fe2e1
Denis GERMAIN <dt.germain@gmail.com>
2023-03-28 11:29:06 +0200

    chore: use fixed nodeport

--- run/9-ingress.sh
+++ run/9-ingress.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 helm repo add traefik https://traefik.github.io/charts
-helm install traefik traefik/traefik
+helm install traefik traefik/traefik \
+              --set ports.web.nodePort=30080 \
+              --set ports.websecure.nodePort=30443
 
 ### Ingress
 cat > ingress.yaml << EOF
