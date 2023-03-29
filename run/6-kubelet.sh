b896ffe1b6fe9ca93b89293f0d4803fc8479483a
Denis GERMAIN <dt.germain@gmail.com>
2023-03-29 13:27:02 +0200

    chore: simplify kubelet startup script

--- run/6-kubelet.sh
+++ run/6-kubelet.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-CONTAINERD_OPTS="--container-runtime=remote \
-                 --container-runtime-endpoint=unix:///var/run/containerd/containerd.sock"
-
-sudo ./kubelet ${CONTAINERD_OPTS} \
---kubeconfig admin.conf \
---register-node=true
+sudo ./kubelet --kubeconfig admin.conf \
+--container-runtime-endpoint=unix:///var/run/containerd/containerd.sock
 
 # If your server has swap (but we should have disabled it)
 #--fail-swap-on=false
