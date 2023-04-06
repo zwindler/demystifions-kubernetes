44f23eba1e5b8a12b7c8e4377438ad6739b3c39c
Denis GERMAIN <dt.germain@gmail.com>
2023-04-06 11:35:03 +0200

    fix: forgot one directory causing ruckus

--- run/0-clean.sh
+++ run/0-clean.sh
@@ -3,11 +3,19 @@
 sudo pkill containerd
 sudo pkill etcd
 
+# remove cluster data
 rm -rf certs/
 rm -rf etcd-data/
-sudo rm -rf /var/lib/kubelet/
+
+# remove binaries
 sudo rm -f /usr/bin/runc
 sudo rm -f /usr/local/bin/kubectl
-sudo rm -rf /opt/cni/bin/
 ./cilium uninstall
+
+# clean directories containing infos from previous installs
+sudo rm -rf /var/lib/kubelet/
+sudo rm -rf /opt/cni/bin/
+sudo rm -rf /etc/cni/net.d/
+
+# remove kubectl in bashrc
 sed -i '/kubectl completion bash/d' ~/.bashrc
