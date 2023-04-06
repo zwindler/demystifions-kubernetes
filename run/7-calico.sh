a86d6d1c7f4b55ce4b4896db2428e35b9b338aed
Denis GERMAIN <dt.germain@gmail.com>
2023-04-06 11:20:06 +0200

    chore: rereadd calico

---
+++ run/7-calico.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+helm repo add projectcalico https://projectcalico.docs.tigera.io/charts
+
+kubectl create namespace tigera-operator
+helm install calico projectcalico/tigera-operator --version v3.24.5 --namespace tigera-operator
