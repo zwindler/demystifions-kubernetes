104534c3ef2a8f6c710034aa25f5d375013317ce
Denis GERMAIN <dt.germain@gmail.com>
2023-04-10 21:42:54 +0200

    chore: remove calico values from script

--- run/8-calico.sh
+++ run/8-calico.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 helm repo add projectcalico https://projectcalico.docs.tigera.io/charts
 
-cat > values.yaml <<EOF
-installation:
-  cni:
-    type: Calico
-  calicoNetwork:
-    bgp: Disabled
-    ipPools:
-    - cidr: 10.244.0.0/16
-      encapsulation: VXLAN
-EOF
-
 kubectl create namespace tigera-operator
-helm install calico projectcalico/tigera-operator --version v3.25.1 --namespace tigera-operator
+helm install calico projectcalico/tigera-operator --version v3.25.1 --namespace tigera-operator -f run/calico-values.yaml 
